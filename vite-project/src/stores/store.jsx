0b17d85e49aa0b0b82a989e34fe2606d21107017
Aden Cronje <adencronje05@gmail.com>
2024-08-16 09:35:56 +0200

    Updated store code slightly

--- vite-project/src/stores/store.jsx
+++ vite-project/src/stores/store.jsx
@@ -3,7 +3,7 @@ import { create } from "zustand";
 const usePreviewStore = create((set) => ({
   // Stores initial state
   // Adding previews data to localstorage
-  previews: JSON.parse(localStorage.getItem("previews")) || null,
+  previews: JSON.parse(localStorage.getItem("previews")) || [],
   error: null,
 
   fetchAllShows: async () => {
